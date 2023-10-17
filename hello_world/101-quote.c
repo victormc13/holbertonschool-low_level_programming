5879e09d26870e52e917b7fd20c6e8cef74c8c4b
victormc13 <viktorsb14@gmail.com>
2023-10-16 21:43:05 -0700

    Add the 101-quote.c program

---
+++ hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/**
+ * main - prints string to the standard error
+ * Return: 1
+ */
+
+int main(void)
+{
+	const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, message, sizeof(message) - 1);
+	return 1;
+}
+	
