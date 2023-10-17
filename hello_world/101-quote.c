8b9353d31e5acc8cd5f57b0774f85dc338f5a7c3
victormc13 <viktorsb14@gmail.com>
2023-10-16 22:07:49 -0700

    Modify the 101-quote.c program to fix betty linter errors

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 	const char message[] =
-		"and that piece of art is useful\"- Dora Korpar, 2015-10-19\n";
+		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 		write(2, message, sizeof(message) - 1);
 		return (1);
