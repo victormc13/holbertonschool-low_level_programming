ef6403e97f0ff6ca26661b521d7d77a36cf5443b
victormc13 <viktorsb14@gmail.com>
2023-10-16 17:36:30 -0700

    Modify the 4-puts.c program

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - print using puts function
+ * Return: 0
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
 
