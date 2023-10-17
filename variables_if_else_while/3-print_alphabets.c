3c5c4516e104f2356d7642ac372d5080efa7fe3b
victormc13 <viktorsb14@gmail.com>
2023-10-17 16:24:24 -0700

    Add the 3-print_alphabets.c program

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lower and upper case
+ * Return: 0
+ */
+
+int main(void)
+{
+	char lowercase = 'a';
+	char uppercase = 'A';
+
+	while (lowercase <= 'z')
+	{
+		putchar(lowercase);
+		lowercase++;
+	}
+	while (uppercase <= 'Z')
+	{
+		putchar(uppercase);
+		uppercase++;
+	}
+	putchar('\n');
+	return (0);
+}
+
