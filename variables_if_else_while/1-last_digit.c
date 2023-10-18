85a72491c4b2f4a5df1e41088b7051bad7d39367
victormc13 <viktorsb14@gmail.com>
2023-10-17 23:19:22 -0700

    Modify the 1-last_digit.c program to fix betty linter error

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -11,6 +11,7 @@ int main(void)
 {
 	int n;
 	int last_digit;
+	char message[] = "Last digit of ";
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
@@ -18,15 +19,13 @@ int main(void)
 	last_digit = n % 10;
 
 	if (last_digit > 5)
-		printf("Last digit of %i is %i and is " -
-				"greater than 5\n", n, last_digit);
+		printf("%s%i is %i and is greater than 5\n", message, n, last_digit);
 
 	else if ((last_digit < 6) && last_digit != 0)
 	{
-		printf("Last digit of %i is %i and is " -
-				"less than 6 and not 0\n", n, last_digit);
+		printf("%s%i is %i and is less than 6 and not 0\n", message, n, last_digit);
 	}
 	else
-		printf("Last digit of %i is %i and is 0\n", n, last_digit);
+		printf("%s%i and is 0\n", message, n);
 	return (0);
 }
