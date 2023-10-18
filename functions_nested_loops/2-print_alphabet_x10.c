619e803abd16dd2d2030080c7fcfac026ab18ec1
victormc13 <viktorsb14@gmail.com>
2023-10-18 15:43:50 -0700

    Modify the 2-print_alphabet_x10.c program

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -7,11 +7,17 @@
 
 void print_alphabet_x10(void)
 {
+	char letter= 'a';
 	int times = 0;
 
 	while (times < 10)
 	{
-		print_alphabet();
+		while (letter <= 'z')
+        	{
+                	_putchar(letter);
+                	letter++;
+        	}
+        	_putchar('\n');
 		times++;
 	}
 }
