d3c797156948e506a0daa5d611923cfdbf6638cf
victormc13 <viktorsb14@gmail.com>
2023-10-18 14:57:35 -0700

    Modify the 1-alphabet.c program to fix betty linter error

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
-/** 
- * main - Print the alphabet, in lowercase, followed by a new line.
+/**
+ * * main - Print the alphabet, in lowercase, followed by a new line.
  * Return: 0
  */
 
 int main(void)
-{	
+{
 	char letter = 'a';
 
 	while (letter < 'z')
