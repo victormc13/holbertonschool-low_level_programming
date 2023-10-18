2f6c00f2d51ca7ba74066eb7583c11e629fe0375
victormc13 <viktorsb14@gmail.com>
2023-10-17 23:58:55 -0700

    Add the 102-print_comb5.c program

---
+++ variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all possible combinations of two two-digit numbers
+ * Return: 0
+ */
+
+int main(void)
+{
+	int tens1, ones1, tens2, ones2;
+
+	for (tens1 = 0; tens1 <= 9; tens1++)
+	{
+		for (ones1 = 0; ones1 <= 9; ones1++)
+		{
+			for (tens2 = tens1; tens2 <= 9; tens2++)
+			{
+				for (ones2 = (tens1 == tens2) ? ones1 + 1 : 0; ones2 <= 9; ones2++)
+				{
+					putchar(tens1 + '0');
+					putchar(ones1 + '0');
+					putchar(' ');
+					putchar(tens2 + '0');
+					putchar(ones2 + '0');
+
+					if (!(tens1 == 9 && ones1 == 9 && tens2 == 9 && ones2 == 9))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
