74dcb328e9171811703070a193abef63cb3e2fe2
victormc13 <viktorsb14@gmail.com>
2023-10-18 17:05:00 -0700

    Add the 3-islower program

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _islower - Checks for lowercse character
+ * @c: Character to be tested
+ * Returns: 1 if c is lowercase, returns 0 otherwise
+ */
+
+int _islower(int c)
+{
+	return (c >='a' && c <= 'z');
+}
