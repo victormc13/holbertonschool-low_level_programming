3bc74572784bb613a61758d1816ba55e10360209
victormc13 <viktorsb14@gmail.com>
2023-10-20 21:12:57 -0700

    Modify the 9-times_table.c program

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -24,8 +24,8 @@ void times_table(void)
 			{
 				_putchar((result / 10) + '0');
 				_putchar((result % 10) + '0');
+				_putchar(',');
 			}
-			_putchar(',');
 			_putchar(' ');
 		}
 		_putchar('\n');
