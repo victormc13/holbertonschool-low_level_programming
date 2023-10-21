c7494292a7b004796923b32967cc031d46703c98
victormc13 <viktorsb14@gmail.com>
2023-10-20 20:44:39 -0700

    Remove the unsigned long format specifier

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -10,13 +10,13 @@ int main(void)
 	char c;
 	int i;
 	long int li;
-	long long int ll;
+	long long int lli;
 	float f;
 
 	printf("Size of a char: %i byte(s)\n", sizeof(c));
 	printf("Size of an int: %i byte(s)\n", sizeof(i));
 	printf("Size of a long int: %i byte(s)\n", sizeof(li));
-	printf("Size of a long long int: %i byte(s)\n", sizeof(ll));
+	printf("Size of a long long int: %i byte(s)\n", sizeof(lli));
 	printf("Size of a float: %i byte(s)\n", sizeof(f));
 	return (0);
 }
