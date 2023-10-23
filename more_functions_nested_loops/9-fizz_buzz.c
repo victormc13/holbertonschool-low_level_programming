1428c63847618b60ab363143c90a0010a1ccd364
victormc13 <viktorsb14@gmail.com>
2023-10-22 23:10:57 -0700

    Add the 9-fizz_buzz.c program

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * fizz_buzz - Prints numbers following the FizzBuzz test
+ * Return: Nothing
+ */
+
+void fizz_buzz(void)
+{
+	int i;
+
+	for (i = 0; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			printf("FizzBuzz");
+		}
+		else
+		{
+			printf("%i", i);
+		}
+	}
+}
