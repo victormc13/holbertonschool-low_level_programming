712b61e085e4c723a0934933126b25e36a48597b
victormc13 <viktorsb14@gmail.com>
2023-10-22 17:10:18 -0700

    Modify the function description

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * print_most_numbers - Prints the numbers, from 0 to 9, followed by a new line.
+ * print_most_numbers - Prints the numbers from 0 to 9, followed by a new line.
  * Description: Don't print 2 and 4
  * Return: Nothing
  */
