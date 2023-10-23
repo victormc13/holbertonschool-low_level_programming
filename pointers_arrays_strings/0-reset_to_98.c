7d7eda3ac34cac67dc1107e4466b6281281e0483
victormc13 <viktorsb14@gmail.com>
2023-10-23 16:36:20 -0700

    Modify the parameter name of the function

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 
 /**
  * reset_to_98 - Updates the value it points to to 98.
- * @*n: integer type pointer as parameter
+ * @n: integer type pointer as parameter
  * Description: Function that takes a pointer to an int as parameter,
  * and updates the value it points to to 98.
  * Return: Nothing
