6cae2e974d1ed777f00d5b6b1fcbc962df6c9989
victormc13 <viktorsb14@gmail.com>
2023-10-23 21:06:10 -0700

    Add the 6-puts2.c function

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts2 - Prints every other character of a string
+ * @str: string as paramater to print every other character
+ * Description: Prints every other character of a string,
+ * starting with the first character, followed by a new line.
+ * Return: Nothing.
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(s[i]);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
