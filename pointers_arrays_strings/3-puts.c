d6d3c2c0abd0495ccc25e1275ed49ad4fe0f3331
victormc13 <viktorsb14@gmail.com>
2023-10-23 17:58:24 -0700

    Add the 3-puts.c function

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts(char *str) - Prints a string, followed by a new line, to `stdout`.
+ * @str: string to be printed
+ * Return: Nothing
+ */
+
+void _puts(char *s)
+{
+	int length = 0;
+
+	while (s[length] != '\0')
+	{
+		_putchar(s[length]);
+	}
+}
