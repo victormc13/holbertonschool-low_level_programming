38c7d651a4aba44848f0f8b8d72a2dc246000758
victormc13 <viktorsb14@gmail.com>
2023-10-23 21:37:54 -0700

    Add the 7-puts_half.c function

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * puts_half - Prints half of a string, followed by a new line.
+ * @str: string took as parameter
+ * Description: The function should print the second half of the string.
+ * If the number of characters is odd, the function should print the last 'n'
+ * characters of the string, where `n = (length_of_the_string - 1) / 2`
+ */
+
+void puts_half(char *str)
+{
+	int length = 0;
+	int start;
+
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+
+	start = length / 2;
+
+	if (length % 2 != 0)
+	{
+		start = (length - 1) / 2;
+	}
+
+	while (str[start] != '\0')
+	{
+		_putchar(str[start]);
+		start++;
+	}
+	_putchar('\n');
+}
+
