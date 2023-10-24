ff2e93e0315471aa5ecfc39564a071c8bb463966
victormc13 <viktorsb14@gmail.com>
2023-10-23 19:16:54 -0700

    Add the 5-rev_string.c function

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rev_string - Reverse a string.
+ * @s: string to be reverse
+ * Return: Reverse string
+ */
+
+void rev_string(char *s)
+{
+	int length = 0;
+	
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+	int start = 0;
+	int end = length - 1;
+
+	while (start < end)
+	{
+		char temp = s[start];
+		s[start] = s[end];
+		s[end] = temp;
+		start++;
+		end--;
+	}
+}
