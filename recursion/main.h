bf78d5fe0e3d50085951db728ba9d5bed325675c
victormc13 <viktorsb14@gmail.com>
2023-10-25 01:03:57 -0500

    Add the 6-is_prime_number.c and helper function prototypes

--- recursion/main.h
+++ recursion/main.h
@@ -8,8 +8,10 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
+int is_prime_number(int n);
 
 
 int _sqrt_getter(int n, int guess); 
+int is_prime_helper(int n, int i);
 
 #endif
