bf42d0d9dbcc7a5a78e332ef3093b54e517746fd
victormc13 <viktorsb14@gmail.com>
2023-10-25 16:35:45 -0700

    Add the 3-mul.c program that multiplies two numbers

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Multiplies two numbers.
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 if everything works, otherwise 1
+ */
+
+int main(int argc, char *argv[])
+{
+	int num1, num2, result;
+
+	if (argc < 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+		result = num1 * num2;
+		printf("%d\n", result);
+	}
+	return (0);
+}
