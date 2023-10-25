4d439fd33f20fe5f41dfc65ead34ea70184ce09a
victormc13 <viktorsb14@gmail.com>
2023-10-25 01:30:59 -0500

    Modify the 100-atoi.c function

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -29,20 +29,11 @@ int _atoi(char *s)
 
 	while (*s >= '0' && *s <= '9')
 	{
-		int digit = *s - '0';
-		
-		if (result > INT_MAX / 10 || (result == INT_MAX / 10 && digit > INT_MAX % 10))
+		if (result > INT_MAX / 10 || (result == INT_MAX / 10 && (*s - '0') > INT_MAX % 10))
 		{
-			if (sign == 1)
-			{
-				return INT_MAX;
-			}
-			else
-			{
-				return INT_MIN;
-			}
+			return ((sign == 1) ? INT_MAX : INT_MIN);
 		}
-		result = result * 10 + digit;
+		result = result * 10 + (*s - '0');
 		s++;
 	}
 	return (result * sign);
