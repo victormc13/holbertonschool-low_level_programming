b6840e04c24086a7ea232e90139e77deee4f31ad
victormc13 <viktorsb14@gmail.com>
2023-10-25 13:58:33 -0700

    Add the 0-whatsmyname.c program

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/** 
+ * main - Prints the name of the program
+ * @argc: argument count
+ * @argv: arguemnt vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+	{
+		printf("%s\n", argv[0]);
+	}
+	return (0);
+}
+ 
