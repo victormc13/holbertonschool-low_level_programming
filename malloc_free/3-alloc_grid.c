e1de65ddd0fb0d7a312042cf7d84905680449bf6
victormc13 <viktorsb14@gmail.com>
2023-10-27 18:15:29 -0700

    Add conditional statement to free allocated memory

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -25,7 +25,16 @@ int **alloc_grid(int width, int height)
 	for (i = 0; i < height; i++)
 	{
 		grid[i] = malloc(width * sizeof(int));
-		
+
+		if (grid[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+			{
+				free (grid[j]);
+			}
+			free (grid);
+			return (NULL);
+		} 
 		for (j = 0; j < width; j++)
 		{
 			grid[i][j] = 0;
