1f589c6e72f608ec532912afce972d3217c58fb3
victormc13 <viktorsb14@gmail.com>
2023-10-27 18:18:57 -0700

    Remove whitespace

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -29,17 +29,16 @@ int **alloc_grid(int width, int height)
 		{
 			for (j = 0; j < i; j++)
 			{
-				free (grid[j]);
+				free(grid[j]);
 			}
-			free (grid);
+			free(grid);
 			return (NULL);
-		} 
+		}
 		for (j = 0; j < width; j++)
 		{
 			grid[i][j] = 0;
 		}
 	}
-	
 	return (grid);
 }
 
