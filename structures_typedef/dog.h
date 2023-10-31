557c58d3d9329168373a20f6ab8790c2c2f8e08e
victormc13 <viktorsb14@gmail.com>
2023-10-30 20:13:11 -0700

    Add the dog.h file that defines a type struct dog with some elements

---
+++ structures_typedef/dog.h
@@ -0,0 +1,13 @@
+/**
+ * struct my_struct - Short description
+ * @a: First member
+ * @b: Second member
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
