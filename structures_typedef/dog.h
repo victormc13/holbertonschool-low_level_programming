9867fa3a5fdd74a2e91b25fd94eb954b5b418eb2
victormc13 <viktorsb14@gmail.com>
2023-10-30 20:31:14 -0700

    Add semicolon at the end of struct statement

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,13 +1,13 @@
 /**
  * struct dog - A type struct dog
  * @name: Name of dog
- * @age: Age of the dog
  * @owner: Owner of the dog
+ * @age: Age of the dog
  */
 
 struct dog
 {
 	char *name;
-	float age;
 	char *owner;
-}
+	float age;
+};
