152341023167b109df72087a4d12314a463bd7b2
victormc13 <viktorsb14@gmail.com>
2023-11-01 12:16:19 -0700

    Remove copy_string function prototype commented code

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -17,7 +17,6 @@ struct dog
 typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-/*char *copy_string(char *str);*/
 dog_t *new_dog(char *name, float age, char *owner);
 
 #endif
