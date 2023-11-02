b80ae2eadbf625fb7af099f35cc180af2fe4cbfc
victormc13 <viktorsb14@gmail.com>
2023-11-01 22:50:41 -0700

    Add betty code style and if conditional statement

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -3,10 +3,12 @@
 /**
  * print_name - Function that prints a name that receives as parameter
  * @name: name to print
+ * @f: function pointer as parameter
  * Return: void
  */
 
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+	if (name != NULL && f != NULL)
+		f(name);
 }
