377b3936ec0b45e0b8325693e38f5a447bfd317e
victormc13 <viktorsb14@gmail.com>
2023-11-02 23:36:27 -0700

    Add the 3-main.c with the main function to execute the entire program

---
+++ function_pointers/3-main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+int main(int argc, char *argv[])
+{
+	int num1 = atoi(argv[1]);
+	char *operator = argv[2];
+	int num2 = atoi(argv[3]);
+	int result;
+
+	int (*operation)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	operation = get_op_func(operator);
+	result = operation(num1, num2);
+	
+	printf("%d\n", result);
+
+	return (0);
+}
