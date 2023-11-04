5fc31ba10f58cd39b87ce1d9077c2d7aeeb5b439
victormc13 <viktorsb14@gmail.com>
2023-11-03 22:57:11 -0700

    Use if/else statement

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -4,23 +4,24 @@
 
 int main(int argc, char *argv[])
 {
-	if (argc != 4)
-	{
-		printf("Error\n");
-		exit(98);
-	}
-
 	int num1 = atoi(argv[1]);
 	char *operator = argv[2];
 	int num2 = atoi(argv[3]);
 	int result;
 
 	int (*operation)(int, int);
-	
-	operation = get_op_func(operator);
-	result = operation(num1, num2);
-	
-	printf("%d\n", result);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	else
+	{
+		operation = get_op_func(operator);
+		result = operation(num1, num2);
+		printf("%d\n", result);
+	}
 
 	return (0);
 }
