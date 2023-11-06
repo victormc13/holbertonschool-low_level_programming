8f80e90b3768dfb5452f1f80bab60db0e6e03aeb
victormc13 <viktorsb14@gmail.com>
2023-11-05 16:24:53 -0800

    Fix betty coding style

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -14,12 +14,12 @@ int sum_them_all(const unsigned int n, ...)
 
 	if (n == 0)
 		return (0);
-	
-	va_start (ap, n);
+
+	va_start(ap, n);
 	for (i = 0; i < n; i++)
 	{
-		sum += va_arg (ap, int);
+		sum += va_arg(ap, int);
 	}
-	va_end (ap);
+	va_end(ap);
 	return (sum);
 }
