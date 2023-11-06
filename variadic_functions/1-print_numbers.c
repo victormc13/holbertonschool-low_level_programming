d7fc28c170d9cc5eaa86318bb90f35250e1b2177
victormc13 <viktorsb14@gmail.com>
2023-11-05 17:08:22 -0800

    Fix typo error

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -19,7 +19,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		number = va_arg(ap, int);
 		printf("%d", number);
-		if (i < n - 1 && separtor != NULL)
+		if (i < n - 1 && separator != NULL)
 		{
 			printf("%s", separator);
 		}
